b388ddf5a62814600e2dd9cbd9c9422288ea00c1
Apu Chakraborty <apu.chakraborty@outlook.com>
2021-11-12 21:49:05 +0530

    wip services

---
+++ pages/signin.js
@@ -0,0 +1,20 @@
+import Login from "../components/Login"
+import Head from 'next/head'
+
+function signin() {
+  return (
+    <>
+      <Head>
+        <title>Login Gdocs</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+    <Login />
+    </>
+  )
+}
+
+export default signin
