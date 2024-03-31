d77e57a1699f9987f05e623f7ce5144117bbc62f
apu chakraborty <apu.chakraborty@outlook.com>
2024-03-31 12:45:43 +0530

    remove key

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAtlHMoq4VwGfkS8rgC10nzmUDz9jGmGWk",
-  authDomain: "gdox-38409.firebaseapp.com",
-  projectId: "gdox-38409",
-  storageBucket: "gdox-38409.appspot.com",
-  messagingSenderId: "689615576765",
-  appId: "1:689615576765:web:3ecab3006aadcaa3fb61d4",
+  // default
 };
 
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
